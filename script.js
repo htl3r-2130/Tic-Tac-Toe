0f271350df1706da37d51888cd27f29bcfed03f9
htl3r-2130 <2130@htl.rennweg.at>
2024-01-02 16:22:40 +0100

    Fertig

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const field_7 = document.querySelector("#field_7");
 const field_8 = document.querySelector("#field_8");
 const field_9 = document.querySelector("#field_9");
 
+let colorX = "red";
+let colorO = "green";
+
 let field_1_value = 0;
 let field_2_value = 0;
 let field_3_value = 0;
@@ -36,6 +39,7 @@ const gameEndStatus = document.querySelector("#outputStatus");
 let startValue=1;
 let gameCounter=0;
 let gameStarted=false;
+let gameEnded = false;
 
 
 const x = document.querySelector("#x-value");
@@ -68,160 +72,179 @@ function setStartValue(indicator) {
 function setSymbol(fieldNumber) {
     let field = "field_" + fieldNumber;
 
-    if (startValue === 1) {
-        switch (field) {
-            case "field_1":
-                if (field_1_value === 0) {
-                    field_1.innerHTML = "X";
-                    field_1.style.color = "orange";
-                    field_1_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_2":
-                if (field_2_value === 0) {
-                    field_2.innerHTML = "X";
-                    field_2.style.color = "orange";
-                    field_2_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_3":
-                if (field_3_value === 0) {
-                    field_3.innerHTML = "X";
-                    field_3.style.color = "orange";
-                    field_3_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_4":
-                if (field_4_value === 0) {
-                    field_4.innerHTML = "X";
-                    field_4.style.color = "orange";
-                    field_4_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_5":
-                if (field_5_value === 0) {
-                    field_5.innerHTML = "X";
-                    field_5.style.color = "orange";
-                    field_5_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_6":
-                if (field_6_value === 0) {
-                    field_6.innerHTML = "X";
-                    field_6.style.color = "orange";
-                    field_6_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_7":
-                if (field_7_value === 0) {
-                    field_7.innerHTML = "X";
-                    field_7.style.color = "orange";
-                    field_7_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_8":
-                if (field_8_value === 0) {
-                    field_8.innerHTML = "X";
-                    field_8.style.color = "orange";
-                    field_8_value = 1;
-                    startValue = 2;
-                }
-                break;
-            case "field_9":
-                if (field_9_value === 0) {
-                    field_9.innerHTML = "X";
-                    field_9.style.color = "orange";
-                    field_9_value = 1;
-                    startValue = 2;
-                }
-                break;
-        }
-    } else if (startValue === 2) {
-        switch (field) {
-            case "field_1":
-                if (field_1_value === 0) {
-                    field_1.innerHTML = "O";
-                    field_1.style.color = "blue";
-                    field_1_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_2":
-                if (field_2_value === 0) {
-                    field_2.innerHTML = "O";
-                    field_2.style.color = "blue";
-                    field_2_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_3":
-                if (field_3_value === 0) {
-                    field_3.innerHTML = "O";
-                    field_3.style.color = "blue";
-                    field_3_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_4":
-                if (field_4_value === 0) {
-                    field_4.innerHTML = "O";
-                    field_4.style.color = "blue";
-                    field_4_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_5":
-                if (field_5_value === 0) {
-                    field_5.innerHTML = "O";
-                    field_5.style.color = "blue";
-                    field_5_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_6":
-                if (field_6_value === 0) {
-                    field_6.innerHTML = "O";
-                    field_6.style.color = "blue";
-                    field_6_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_7":
-                if (field_7_value === 0) {
-                    field_7.innerHTML = "O";
-                    field_7.style.color = "blue";
-                    field_7_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_8":
-                if (field_8_value === 0) {
-                    field_8.innerHTML = "O";
-                    field_8.style.color = "blue";
-                    field_8_value = 2;
-                    startValue = 1;
-                }
-                break;
-            case "field_9":
-                if (field_9_value === 0) {
-                    field_9.innerHTML = "O";
-                    field_9.style.color = "blue";
-                    field_9_value = 2;
-                    startValue = 1;
-                }
-                break;
+    if (gameEnded===false) {
+        if (startValue === 1) {
+            switch (field) {
+                case "field_1":
+                    if (field_1_value === 0) {
+                        field_1.innerHTML = "X";
+                        field_1.style.color = colorX;
+                        field_1_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_2":
+                    if (field_2_value === 0) {
+                        field_2.innerHTML = "X";
+                        field_2.style.color = colorX;
+                        field_2_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_3":
+                    if (field_3_value === 0) {
+                        field_3.innerHTML = "X";
+                        field_3.style.color = colorX;
+                        field_3_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_4":
+                    if (field_4_value === 0) {
+                        field_4.innerHTML = "X";
+                        field_4.style.color = colorX;
+                        field_4_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_5":
+                    if (field_5_value === 0) {
+                        field_5.innerHTML = "X";
+                        field_5.style.color = colorX;
+                        field_5_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_6":
+                    if (field_6_value === 0) {
+                        field_6.innerHTML = "X";
+                        field_6.style.color = colorX;
+                        field_6_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_7":
+                    if (field_7_value === 0) {
+                        field_7.innerHTML = "X";
+                        field_7.style.color = colorX;
+                        field_7_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_8":
+                    if (field_8_value === 0) {
+                        field_8.innerHTML = "X";
+                        field_8.style.color = colorX;
+                        field_8_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_9":
+                    if (field_9_value === 0) {
+                        field_9.innerHTML = "X";
+                        field_9.style.color = colorX;
+                        field_9_value = 1;
+                        startValue = 2;
+                        gameCounter+=1;
+                    }
+                    break;
+            }
+        } else if (startValue === 2) {
+            switch (field) {
+                case "field_1":
+                    if (field_1_value === 0) {
+                        field_1.innerHTML = "O";
+                        field_1.style.color = colorO;
+                        field_1_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_2":
+                    if (field_2_value === 0) {
+                        field_2.innerHTML = "O";
+                        field_2.style.color = colorO;
+                        field_2_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_3":
+                    if (field_3_value === 0) {
+                        field_3.innerHTML = "O";
+                        field_3.style.color = colorO;
+                        field_3_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_4":
+                    if (field_4_value === 0) {
+                        field_4.innerHTML = "O";
+                        field_4.style.color = colorO;
+                        field_4_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_5":
+                    if (field_5_value === 0) {
+                        field_5.innerHTML = "O";
+                        field_5.style.color = colorO;
+                        field_5_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_6":
+                    if (field_6_value === 0) {
+                        field_6.innerHTML = "O";
+                        field_6.style.color = colorO;
+                        field_6_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_7":
+                    if (field_7_value === 0) {
+                        field_7.innerHTML = "O";
+                        field_7.style.color = colorO;
+                        field_7_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_8":
+                    if (field_8_value === 0) {
+                        field_8.innerHTML = "O";
+                        field_8.style.color = colorO;
+                        field_8_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+                case "field_9":
+                    if (field_9_value === 0) {
+                        field_9.innerHTML = "O";
+                        field_9.style.color = colorO;
+                        field_9_value = 2;
+                        startValue = 1;
+                        gameCounter+=1;
+                    }
+                    break;
+            }
         }
     }
     if (!(startValue===0)) {
         gameStarted=true;
-        gameCounter+=1;
         checkWin();
     }
 }
@@ -237,6 +260,7 @@ function checkWin() {
         (field_2_value === 1 && field_5_value === 1 && field_8_value === 1) ||
         (field_3_value === 1 && field_6_value === 1 && field_9_value === 1)
     ) {
+        gameEnded=true;
         gameEnd.style.display = "flex";
         gameEndStatus.innerHTML = "X hat gewonnen!";
     } else if (
@@ -249,9 +273,11 @@ function checkWin() {
         (field_2_value === 2 && field_5_value === 2 && field_8_value === 2) ||
         (field_3_value === 2 && field_6_value === 2 && field_9_value === 2)
     ) {
+        gameEnded=true;
         gameEnd.style.display = "flex";
         gameEndStatus.innerHTML = "O hat gewonnen!";
     } else if (gameCounter === 9) {
+        gameEnded=true;
         gameEnd.style.display = "flex";
         gameEndStatus.innerHTML = "Unentschieden!";
     }
@@ -288,4 +314,5 @@ function reset() {
     gameEnd.style.display = "none";
     gameEndStatus.innerHTML = "";
     setStartValue(1);
+    gameEnded=false;
 }
